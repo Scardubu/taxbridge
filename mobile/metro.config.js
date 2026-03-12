4d195850b07e1584fb933562999fd39268c3f8f3
scardubu <scardubu@gmail.com>
2026-03-12 11:53:24 +0100

    env edit

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
-const config = getDefaultConfig(__dirname);
+
+// Use file:// URL for Windows compatibility
+const projectRoot = path.resolve(__dirname);
+const config = getDefaultConfig(projectRoot);
 
 config.cacheStores = [];
 config.resetCache = true;
